aae0ed59666639e9d1ed54dfe24091a060fb5122
AmirSolt <killop1997@gmail.com>
2023-03-22 10:47:36 -0400

    added groups

--- widget/app.js
+++ widget/app.js
@@ -1,5 +1,20 @@
 const root = document.getElementById('root');
 
+
+
+
+class Group {
+    constructor(class_name) {
+        this.class_name = class_name;
+        this.element = document.createElement('div');
+        this.element.classList.add("group");
+        this.element.classList.add(this.class_name);
+    }
+}
+
+
+
+
 class Button {
     constructor(class_name, text, task) {
         this.class_name = class_name;
@@ -42,10 +57,50 @@ class Image {
 
 
 
-let img = new Image('img', 'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png');
-let text = new Text('text', 'Hello World');
-let button = new Button('buy-button', 'Buy', () => { console.log('Buy') });
+class TextField {
+    constructor(class_name, placeholder) {
+        this.class_name = class_name;
 
-root.appendChild(img.element);
-root.appendChild(text.element);
-root.appendChild(button.element);
+        this.element = document.createElement('input');
+        this.element.type = "text";
+        this.element.placeholder = placeholder;
+        this.element.classList.add("text-field");
+        this.element.classList.add(this.class_name);
+    }
+}
+
+
+
+let group_1 = new Group('group');
+let img = new Image('img', 'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png');
+let title = new Text('text', 'Shoes');
+let description = new Text('text', 'Best shoes in the world!');
+let price = new Text('text', '$100');
+let order_button = new Button('buy-button', 'Order Now', () => { console.log('Buy') });
+
+group_1.element.appendChild(img.element);
+group_1.element.appendChild(title.element);
+group_1.element.appendChild(description.element);
+group_1.element.appendChild(price.element);
+group_1.element.appendChild(order_button.element);
+
+
+
+
+let group_2 = new Group('group');
+let email_field = new TextField('text-field', 'Enter your email');
+let number_field = new TextField('text-field', 'Enter your number');
+let name_field = new TextField('text-field', 'Enter your name');
+let address_field = new TextField('text-field', 'Enter your name');
+let submit_info_button = new Button('submit-button', 'Submit', () => { console.log('Submit Info') });
+
+
+group_2.element.appendChild(email_field.element);
+group_2.element.appendChild(number_field.element);
+group_2.element.appendChild(name_field.element);
+group_2.element.appendChild(address_field.element);
+group_2.element.appendChild(submit_info_button.element);
+
+
+root.appendChild(group_1.element);
+root.appendChild(group_2.element);
